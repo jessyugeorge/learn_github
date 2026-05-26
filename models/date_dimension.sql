4c799090c27e4afd34bfcdedde42d5b794bf1fac
jessyug@gmail.com <jessyug@gmail.com>
2026-05-26 02:35:09 +0000

    daily_weather

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -5,16 +5,11 @@ WITH CTE AS
     date(to_timestamp(started_at)) as date_started_at,
     hour(to_timestamp(started_at)) as hour_started_at,
 
-    CASE WHEN DAYNAME(to_timestamp(started_at)) IN ('SAT','SUN')
-        THEN 'WEEKEND'
-        ELSE 'BUSINESSDAY'
-    END AS DAY_TYPE,
+    {{day_type('started_at')}} AS DAY_TYPE,
 
-    CASE WHEN MONTH(to_timestamp(started_at)) IN (12,1,2) THEN 'WINTER'
-    WHEN MONTH(to_timestamp(started_at)) IN (3,4,5) THEN 'SPRING'
-    WHEN MONTH(to_timestamp(started_at)) IN (6,7,8) THEN 'SUMMER'
-    ELSE 'AUTUMN'
-    END AS SEASON_OF_YEAR
+    {{get_season('started_at')}} AS SEASON_OF_YEAR,
+
+    {{function1('started_at')}} as HISTORY
     FROM {{ source('demo', 'bike') }}
 )
 select * from CTE
